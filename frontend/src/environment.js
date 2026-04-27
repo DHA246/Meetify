bbdc1a9f1e884aa9b7d1276c1f8e010c696c1d17
DHA246 <dhanurmudgal000@gmail.com>
2026-04-27 19:41:40 +0530

    Initial commit

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,8 @@
-let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+let IS_PROD = false;
+const server = "http://localhost:8000"
+    
 
-    "http://localhost:8000"
+    
 
 
 export default server;
